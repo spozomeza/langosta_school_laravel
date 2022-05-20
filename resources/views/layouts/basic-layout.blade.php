24e5bfc00279a2b0a67df4f67b789cca53afe2ad
JuanCarlosRoman <jromanam@uoc.edu>
2022-05-20 14:48:12 +0200

    Integraciones del producto 2: css, inicio, logo

---
+++ resources/views/layouts/basic-layout.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&family=Paytone+One&display=swap" rel="stylesheet">
+    <!-- Libreria de iconos de FontAwsome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />   
+    <!-- Hoja de estilos css  -->
+    <link href="{{ asset('css\styles.css') }}" rel="stylesheet">
+
+    <title>Langosta.School</title>
+
+</head>
+<body>
+
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg nav-langosta">
+    <div class="container">
+        <a class="navbar-brand" href="/">
+            <img src="{{ asset('img\logo_langosta.png') }}" alt="" width="35" height="35" class="d-inline-block align-text-top">
+            Langosta.School
+        </a>
+    </div>
+</nav>
+
+
+<!-- Contenido -->
+@yield('content')
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+
+
+<!-- Footer -->
+<footer class="text-center text-white fixed-bottom footer-langosta">
+    <!-- Copyright -->
+    <div class="text-center p-3">© 2022 Langosta.school</div>
+    <!-- Copyright -->
+</footer>
+
+</html>
