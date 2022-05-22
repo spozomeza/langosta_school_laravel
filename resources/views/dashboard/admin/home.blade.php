da422ac295ea31338caee43503101914483e2c0a
bkuchel <100967866+bkuchel@users.noreply.github.com>
2022-05-22 22:57:28 +0200

    Vista panel admin

--- resources/views/dashboard/admin/home.blade.php
+++ resources/views/dashboard/admin/home.blade.php
@@ -1,3 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container text-center">
+    <h1 class="mb-5 mt-5">Panel de administración</h1>
+    <div class="row">
+        <div class="col-md-8 mx-auto">
+        <div class="grid-card">
+        <a href="">
+            <div class="menu-admin-opt p-4">
+                <i class="bi bi-mortarboard-fill"></i> Alumnos
+            </div>
+        </a>
+        <a href="/teachers">
+            <div class="menu-admin-opt p-4">
+                <i class="bi bi-briefcase-fill"></i> Profesores
+            </div>
+        </a>
+        <a href="/courses">
+            <div class="menu-admin-opt p-4">
+                <i class="bi bi-clipboard-check"></i> Cursos
+            </div>
+        </a>        
+        <a href="/enrollments">
+            <div class="menu-admin-opt p-4">
+                <i class="bi bi-journal-text"></i> Matrículas
+            </div>
+        </a>        
+        <a href="/schedules">
+            <div class="menu-admin-opt p-4">
+                <i class="bi bi-alarm-fill"></i> Horarios
+            </div>
+        </a>
+        <a href="/classes">
+            <div class="menu-admin-opt p-4">
+                <i class="bi bi-easel"></i> Clases
+            </div>
+        </a>
+    </div>
+
+    </div>
+</div>
+</div>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -40,3 +84,4 @@
 </div>
 </body>
 </html>
+@endsection
