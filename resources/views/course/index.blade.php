f4bc201cb03339ac0a99b05a1c73b59db65419cd
bkuchel <100967866+bkuchel@users.noreply.github.com>
2022-05-22 23:53:59 +0200

    Dashboard admin view corregida

--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -1,83 +1,55 @@
 @extends('layouts.app')
 
-@section('template_title')
-    Course
-@endsection
-
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
-                    <div class="card-header">
-                        <div style="display: flex; justify-content: space-between; align-items: center;">
-
-                            <span id="card_title">
-                                {{ __('Course') }}
-                            </span>
-
-                             <div class="float-right">
-                                <a href="{{ route('admin.courses.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
-                                  {{ __('Create New') }}
-                                </a>
-                              </div>
-                        </div>
-                    </div>
-                    @if ($message = Session::get('success'))
-                        <div class="alert alert-success">
-                            <p>{{ $message }}</p>
-                        </div>
-                    @endif
-
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover">
-                                <thead class="thead">
-                                    <tr>
-                                        <th>No</th>
-
-										<th>Name</th>
-										<th>Description</th>
-										<th>Date Start</th>
-										<th>Date End</th>
-										<th>Updated At</th>
-										<th>Created At</th>
-										<th>Active</th>
-
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($courses as $course)
-                                        <tr>
-                                            <td>{{ ++$i }}</td>
-
-											<td>{{ $course->name }}</td>
-											<td>{{ $course->description }}</td>
-											<td>{{ $course->date_start }}</td>
-											<td>{{ $course->date_end }}</td>
-											<td>{{ $course->updated_at }}</td>
-											<td>{{ $course->created_at }}</td>
-											<td>{{ $course->active }}</td>
-
-                                            <td>
-                                                <form action="{{ route('admin.courses.destroy',$course->id) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('admin.courses.show',$course->id) }}"><i class="fa fa-fw fa-eye"></i> Show</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('admin.courses.edit',$course->id) }}"><i class="fa fa-fw fa-edit"></i> Edit</a>
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> Delete</button>
-                                                </form>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-                {!! $courses->links() !!}
-            </div>
-        </div>
+<!-- Inicio Body -->
+<div class="container text-center">
+    <h1 class="mb-5 mt-5">Cursos</h1>
+    <!-- El contenido va aqui -->
+    <div class="d-flex justify-content-end mb-3">
+        <a href="{{ route('admin.courses.create') }}">
+            <div class="nuevo-registro"><i class="bi bi-plus-circle"></i> Nuevo curso</div>
+        </a>
     </div>
+    @if ($message = Session::get('success'))
+    <div class="alert alert-success">
+        <p>{{ $message }}</p>
+    </div>
+    @endif
+
+    <!--tabla con los contenidos a mostrar-->
+    <table id="tabla" class="table text-center">
+        <thead class="thead-light">
+            <tr>
+                <th style="border-top-left-radius:10px;">ID</th>
+                <th>Nombre del curso</th>
+                <th>Descripción</th>
+                <th>Fecha inicio</th>
+                <th>Fecha fin</th>
+                <th>Estado</th>
+                <th style="border-top-right-radius:10px;"> Acciones</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            @foreach ($courses as $course)
+            <tr>
+                <td>{{ ++$i }}</td>
+                <td>{{ $course->name }}</td>
+                <td>{{ $course->description }}</td>
+                <td>{{ $course->date_start }}</td>
+                <td>{{ $course->date_end }}</td>
+                <td>{{ $course->active }}</td>
+                <td>
+                    <form action="{{ route('admin.courses.destroy',$course->id) }}" method="POST">
+                        <a class="btn btn-sm btn-primary " href="{{ route('admin.courses.show',$course->id) }}"><i class="fa fa-fw fa-eye"></i> Show</a>
+                        <a href="{{ route('admin.courses.edit',$course->id) }}"><i class="bi bi-pencil-square icono mr-1"></i></a>
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger btn-sm" style="background-color:transparent;"><i class="bi bi-trash3-fill icono ml-1"></i></button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+
+
 @endsection
--- resources/views/dashboard/admin/home.blade.php
+++ resources/views/dashboard/admin/home.blade.php
@@ -16,22 +16,21 @@
                 <i class="bi bi-briefcase-fill"></i> Profesores
             </div>
         </a>
-        <a href="/courses">
+        <a href="{{ route('admin.courses.index') }}">
             <div class="menu-admin-opt p-4">
                 <i class="bi bi-clipboard-check"></i> Cursos
             </div>
-        </a>        
-        <a href="/enrollments">
+        <a href="{{ route('admin.enrollments.index') }}" >
             <div class="menu-admin-opt p-4">
                 <i class="bi bi-journal-text"></i> Matrículas
             </div>
         </a>        
-        <a href="/schedules">
+        <a href="{{ route('admin.schedules.index') }}">
             <div class="menu-admin-opt p-4">
                 <i class="bi bi-alarm-fill"></i> Horarios
             </div>
         </a>
-        <a href="/classes">
+        <a href="{{ route('admin.clases.index') }}">
             <div class="menu-admin-opt p-4">
                 <i class="bi bi-easel"></i> Clases
             </div>
@@ -41,50 +40,4 @@
     </div>
 </div>
 </div>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Admin Dashboard | Home</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-<div class="container">
-    <div class="row">
-        <div class="col-md-6 offset-md-3" style="margin-top: 45px">
-            <h4>Perfil Admin?</h4><hr>
-            <table class="table table-striped table-inverse table-responsive">
-                <thead class="thead-inverse">
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td scope="row">{{ Auth::guard('admin')->user()->name }}</td>
-                    <td>{{ Auth::guard('admin')->user()->email }}</td>
-                    <td>{{ Auth::guard('admin')->user()->phone }}</td>
-                    <td>
-                        <a href="{{ route('admin.logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a>
-                        <form action="{{ route('admin.logout') }}" id="logout-form" method="post">@csrf</form>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
-<<<<<<< HEAD
-</body>
-</html>
-=======
-
-
->>>>>>> ce251d731c241e56d7f4d612fddf449d91a33dee
 @endsection
