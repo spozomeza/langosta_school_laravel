f4c92a9b130f93b7426ba38d51059f760be61ae0
JuanCarlosRoman <jromanam@uoc.edu>
2022-05-22 22:08:52 +0200

    sincronizar

--- resources/views/dashboard/admin/home.blade.php
+++ resources/views/dashboard/admin/home.blade.php
@@ -1,19 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Admin Dashboard | Home</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+@extends('layouts.basic-layout')
 
-</head>
-<body style="background-color: #d7dadb">
+@section('content')
+
 <div class="container">
     <div class="row">
         <div class="col-md-6 offset-md-3" style="margin-top: 45px">
-            <h4>Admin Dashboard</h4><hr>
+            <h4>Perfil Admin?</h4><hr>
             <table class="table table-striped table-inverse table-responsive">
                 <thead class="thead-inverse">
                 <tr>
@@ -38,5 +30,6 @@
         </div>
     </div>
 </div>
-</body>
-</html>
+
+
+@endsection
