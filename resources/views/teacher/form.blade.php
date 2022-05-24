55f61d514b4641adf628b9e39dbd64147e6dfa60
JuanCarlosRoman <jromanam@uoc.edu>
2022-05-24 19:51:39 +0200

    form teacher front actualizado

--- resources/views/teacher/form.blade.php
+++ resources/views/teacher/form.blade.php
@@ -1,45 +1,72 @@
-<div class="box box-info padding-1">
-    <div class="box-body">
+<div class="row justify-content-center">
+    <div class="col-md-4 col-sm-12">
+        <div class="form-group">
+
+            <div class="mb-2">
+                <div class="d-flex mb-2">
+                    <label for="name">Nombre del profesor</label>
+                </div>
+                {{ Form::text('name', $teacher->name, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Name']) }}
+                {!! $errors->first('name', '<div class="invalid-feedback">:message</div>') !!}
+            </div>
+        </div>
 
         <div class="form-group">
-            {{ Form::label('name') }}
-            {{ Form::text('name', $teacher->name, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Name']) }}
-            {!! $errors->first('name', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-        <div class="form-group">
-            {{ Form::label('email') }}
+            <div class="d-flex mb-2">
+                <label for="email">Email</label>
+            </div>
             {{ Form::email('email', $teacher->email, ['class' => 'form-control' . ($errors->has('email') ? ' is-invalid' : ''), 'placeholder' => 'Email']) }}
             {!! $errors->first('email', '<div class="invalid-feedback">:message</div>') !!}
         </div>
+
         <div class="form-group">
-            {{ Form::label('nif') }}
+            <div class="d-flex mb-2">
+                <label for="nif">NIF</label>
+            </div>
             {{ Form::text('nif', $teacher->nif, ['class' => 'form-control' . ($errors->has('nif') ? ' is-invalid' : ''), 'placeholder' => 'Nif']) }}
             {!! $errors->first('nif', '<div class="invalid-feedback">:message</div>') !!}
         </div>
+
         <div class="form-group">
-            {{ Form::label('surname') }}
+            <div class="d-flex mb-2">
+                <label for="surname">Apellido</label>
+            </div>
             {{ Form::text('surname', $teacher->surname, ['class' => 'form-control' . ($errors->has('surname') ? ' is-invalid' : ''), 'placeholder' => 'Surname']) }}
             {!! $errors->first('surname', '<div class="invalid-feedback">:message</div>') !!}
         </div>
+
         <div class="form-group">
-            {{ Form::label('telephone') }}
+            <div class="d-flex mb-2">
+                <label for="telephone">Teléfono</label>
+            </div>
             {{ Form::text('telephone', $teacher->telephone, ['class' => 'form-control' . ($errors->has('telephone') ? ' is-invalid' : ''), 'placeholder' => 'Telephone']) }}
             {!! $errors->first('telephone', '<div class="invalid-feedback">:message</div>') !!}
         </div>
+
         <div class="form-group">
-            {{ Form::label('date_registered') }}
+            <div class="d-flex mb-2">
+                <label for="date_registered">Fecha de registro</label>
+            </div>
             {{ Form::date('date_registered', $teacher->date_registered, ['class' => 'form-control' . ($errors->has('date_registered') ? ' is-invalid' : ''), 'placeholder' => 'Date Registered']) }}
             {!! $errors->first('date_registered', '<div class="invalid-feedback">:message</div>') !!}
         </div>
 
         <div class="form-group">
-            {{ Form::label('Password') }}
+            <div class="d-flex mb-2">
+                <label for="password">Contraseña</label>
+            </div>
             {{ Form::password('password', $teacher->password, ['class' => 'form-control' . ($errors->has('date_registered') ? ' is-invalid' : ''), 'placeholder' => 'Password']) }}
             {!! $errors->first('password', '<div class="invalid-feedback">:message</div>') !!}
         </div>
 
-    </div>
-    <div class="box-footer mt20">
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <div class="form-group mt-5">
+            <button class="nuevo-registro form-control" type="submit" value="submit">Crear profesor</button>
+        </div>
+
+        <div class="form-group mb-5">
+            <div class="btn form-control">
+                <a class="link-sesion" href="{{ route('admin.courses.index') }}"> Cancelar</a>
+            </div>
+        </div>
     </div>
 </div>
