f3716c96952a71db6c21dae9e10a43883586938e
JuanCarlosRoman <jromanam@uoc.edu>
2022-05-24 18:59:10 +0200

    actualización front end

--- resources/views/dashboard/teacher/home.blade.php
+++ resources/views/dashboard/teacher/home.blade.php
@@ -2,140 +2,45 @@
 
 @section('content')
 
+<div class="container text-center">
+    <h1 class="mb-5 mt-5">Panel del profesor</h1>
+    <div class="row">
+        <div class="col-md-8 mx-auto">
+            <div class="grid-card">
+                <a href="{{ route('admin.courses.index') }}">
+                    <div class="menu-admin-opt p-4">
+                        <i class="bi bi-clipboard-check"></i> Cursos
+                    </div>
+                </a>  
+                <a href="{{ route('admin.courses.index') }}">
+                    <div class="menu-admin-opt p-4">
+                        <i class="bi bi-book"></i> Asignaturas
+                    </div>
+                </a>  
+                <a href="{{ route('admin.courses.index') }}">
+                    <div class="menu-admin-opt p-4">
+                        <i class="bi bi-journal-bookmark-fill"></i> Trabajos
+                    </div>
+                </a>  
+                <a href="{{ route('admin.courses.index') }}">
+                    <div class="menu-admin-opt p-4">
+                        <i class="bi bi-ui-checks"></i> Exámenes
+                    </div>
+                </a>  
+                <a href="{{ route('admin.users.index') }}">
+                    <div class="menu-admin-opt p-4">
+                        <i class="bi bi-mortarboard-fill"></i> Alumnos
+                    </div>
+                </a>      
 
-<div class="container">
-    <br />
-    <h1 class="text-center text-primary"><u>Teacher Calendar</u></h1>
-    <br />
 
-    <div id="calendar"></div>
-
+            </div>
+        </div>
+    </div>
 </div>
 
-<script>
-
-    $(document).ready(function () {
-
-        $.ajaxSetup({
-            headers:{
-                'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-
-        var calendar = $('#calendar').fullCalendar({
-            editable:true,
-            header:{
-                left:'prev,next today',
-                center:'title',
-                right:'month,agendaWeek,agendaDay'
-            },
-            events:'/full-calender',
-            selectable:true,
-            selectHelper: true,
-            select:function(start, end, allDay)
-            {
-                var title = prompt('Event Title:');
-
-                if(title)
-                {
-                    var start = $.fullCalendar.formatDate(start, 'Y-MM-DD HH:mm:ss');
-
-                    var end = $.fullCalendar.formatDate(end, 'Y-MM-DD HH:mm:ss');
-
-                    $.ajax({
-                        url:"/full-calender/action",
-                        type:"POST",
-                        data:{
-                            title: title,
-                            start: start,
-                            end: end,
-                            type: 'add'
-                        },
-                        success:function(data)
-                        {
-                            calendar.fullCalendar('refetchEvents');
-                            alert("Event Created Successfully");
-                        }
-                    })
-                }
-            },
-            editable:true,
-            eventResize: function(event, delta)
-            {
-                var start = $.fullCalendar.formatDate(event.start, 'Y-MM-DD HH:mm:ss');
-                var end = $.fullCalendar.formatDate(event.end, 'Y-MM-DD HH:mm:ss');
-                var title = event.title;
-                var id = event.id;
-                $.ajax({
-                    url:"/full-calender/action",
-                    type:"POST",
-                    data:{
-                        title: title,
-                        start: start,
-                        end: end,
-                        id: id,
-                        type: 'update'
-                    },
-                    success:function(response)
-                    {
-                        calendar.fullCalendar('refetchEvents');
-                        alert("Event Updated Successfully");
-                    }
-                })
-            },
-            eventDrop: function(event, delta)
-            {
-                var start = $.fullCalendar.formatDate(event.start, 'Y-MM-DD HH:mm:ss');
-                var end = $.fullCalendar.formatDate(event.end, 'Y-MM-DD HH:mm:ss');
-                var title = event.title;
-                var id = event.id;
-                $.ajax({
-                    url:"/full-calender/action",
-                    type:"POST",
-                    data:{
-                        title: title,
-                        start: start,
-                        end: end,
-                        id: id,
-                        type: 'update'
-                    },
-                    success:function(response)
-                    {
-                        calendar.fullCalendar('refetchEvents');
-                        alert("Event Updated Successfully");
-                    }
-                })
-            },
-
-            eventClick:function(event)
-            {
-                if(confirm("Are you sure you want to remove it?"))
-                {
-                    var id = event.id;
-                    $.ajax({
-                        url:"/full-calender/action",
-                        type:"POST",
-                        data:{
-                            id:id,
-                            type:"delete"
-                        },
-                        success:function(response)
-                        {
-                            calendar.fullCalendar('refetchEvents');
-                            alert("Event Deleted Successfully");
-                        }
-                    })
-                }
-            }
-        });
-
-    });
-
-</script>
 
 
 @endsection
 
 
-<!-- </body>
-</html> -->
